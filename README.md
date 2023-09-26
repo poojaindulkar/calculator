9b7518ffa043eede55a1b955d2e76988ba24332b
Pooja Indulkar <poojaindulkar200@gmail.com>
2023-09-27 00:06:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# calculator
+# Simple React Calculator
+
+![Calculator Demo](demo.gif)
+
+## Key Features:
+
+- **Responsive UI:** The calculator provides a user-friendly interface for easy interaction.
+- **Dynamic Input Handling:** It dynamically handles changes in input.
+- **Mathematical Operations:** Supports a range of mathematical operations, including addition, subtraction, multiplication, and division.
+- **React Fundamentals:** Demonstrates fundamental React concepts such as re-rendering, state management, and component-based architecture.
+- **Extensible:** Easily extend the functionality to add new features.
+
+## Getting Started:
+
+1. **Clone the Repository:**
+git clone https://github.com/poojaindulkar/calculator.git
+2. **Install Dependencies:**
+npm install
+3. **Start the Development Server:**
+npm start
+
+4. **Open in Browser:**
+Open your web browser and navigate to [http://localhost:3000](http://localhost:3000).
+
+## Usage:
+
+- **Performing Operations:**
+- Enter numbers and use the provided buttons for basic arithmetic operations (+, -, *, /).
+
+- **Real-time Updates:**
+- Enjoy a seamless user experience with real-time updates.
+
+![Calculator Usage Demo](usage-demo.gif)
+
---
+++ src/App.js
@@ -0,0 +1,145 @@
+import { useReducer } from "react";
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+
+export const ACTIONS={
+  ADD_DIGIT:'add-digit',
+  CHOOSE_OPERATION:'choose-operation',
+  CLEAR:'clear',
+  DELETE_DIGIT:'delete-digit',
+  EVALUATE:'evaluate'
+}
+
+function evaluate({currentOperand,previousOperand,operation}){
+  const prev=parseInt(previousOperand)
+  const curr=parseInt(currentOperand)
+  if(isNaN(prev)|| isNaN(curr)) return previousOperand
+  let computation=""
+  switch(operation){
+    case "/":
+      computation=prev/curr
+      break
+    case "*":
+      computation=prev*curr
+      break
+    case "+":
+      computation=prev+curr
+      break
+    case "-":
+      computation=prev-curr
+      break
+  }
+  return computation.toString()
+}
+function reducer(state,{type,payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      if(state.overwrite){
+        return{
+          ...state,
+          overwrite:false,
+          currentOperand: payload.digit,
+        }
+      }
+      if(payload.digit==="0" && state.currentOperand === "0") return state;
+      if(payload.digit==="." && state.currentOperand.includes(".")) return state;
+      return{
+        ...state,// used to create a copy of the current state object.
+        currentOperand: `${state.currentOperand ||""}${payload.digit}`,
+      }
+
+
+    case ACTIONS.CLEAR:
+      return {}
+
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if(state.currentOperand == null && state.previousOperand == null) return state;
+      if(state.previousOperand==null){
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        }
+      }
+      return{
+        ...state,
+        previousOperand: evaluate(state),
+        currentOperand: null,
+        operation: payload.operation
+      }
+
+    
+    case ACTIONS.DELETE_DIGIT:
+      if(state.overwrite) return {}
+      
+      return{
+        ...state,
+        currentOperand: state.currentOperand.slice(0,-1)
+      }
+
+    
+    case ACTIONS.EVALUATE:
+      if(state.currentOperand == null ||
+        state.previousOperand == null ||
+        state.operation == null ){
+          return state
+        }
+
+      return{
+        ...state,
+        overwrite: true,
+        currentOperand: evaluate(state),
+        operation: null,
+        previousOperand: null
+      }
+  }
+
+}
+
+const INTEGER_FORMARTTER =  new Intl.NumberFormat("en-us",{
+  maximumFractionDigits:0
+})
+function formatOperand(operand){
+  if(operand==null) return 
+  const[integer,decimal]=operand.split(".")
+  if(decimal==null)return INTEGER_FORMARTTER.format(integer)
+  return `${INTEGER_FORMARTTER.format(integer)}.${decimal}`
+}
+function App() {
+  const [{currentOperand, previousOperand, operation},dispatch]=useReducer(reducer,{});
+
+  return (
+    <div className='claulator-grid'>
+      <div className='output'>
+        <div className='previous-output'>{formatOperand(previousOperand)} {operation}</div>
+        <div className='current-output'>{formatOperand(currentOperand)}</div>
+      </div>
+      <button className='span-two' onClick={()=>dispatch({type: ACTIONS.CLEAR})}>AC</button>
+      <button onClick={()=>dispatch({type: ACTIONS.DELETE_DIGIT})}>DEL</button>
+      <OperationButton operation="/" dispatch={dispatch}/>
+
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
+      <OperationButton operation="*" dispatch={dispatch}/>
+
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
+      <OperationButton operation="+" dispatch={dispatch}/>
+
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
+      <OperationButton operation="-" dispatch={dispatch}/>
+
+      <DigitButton digit="." dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
+      <button className='span-two' onClick={()=>dispatch({type: ACTIONS.EVALUATE})}>=</button>
+    </div>
+  );
+}
+
+export default App;
